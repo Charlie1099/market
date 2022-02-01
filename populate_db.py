b43324790e787486b82b65bb97d0df87cfa6c36b
charlie1099 <cdcharlesworth@gmail.com>
2022-01-31 21:09:20 -0700

    added alpaca api

--- populate_db.py
+++ populate_db.py
@@ -1,10 +1,19 @@
 import sqlite3
+import alpaca_trade_api as tradeapi
 
 connection = sqlite3.connect('app.db')
 
 cursor = connection.cursor()
 
-cursor.execute("DELETE FROM stock")
+api = tradeapi.REST('PKQOHLLVJP3GN8MUP1FI', 'vMy9ecXQP640EMXbreGhYdrkSH4Kz7hmgeTQpP6a', base_url='https://paper-api.alpaca.markets') # or use ENV Vars shown below
+assets = api.list_assets()
 
+for asset in assets:
+    try:
+        if asset.status == 'active' and asset.tradable:
+            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
+    except Exception as e:
+        print(asset.symbol)
+        print(e)
 
 connection.commit()
